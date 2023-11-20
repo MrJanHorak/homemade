7155401049400e378df1f2e35d18bdde1152e19b
MrJanHorak <janny_b_goode@hotmail.com>
2023-11-20 18:52:49 -0500

    add project model

---
+++ models/project.js
@@ -0,0 +1,105 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const projectComment = new Schema(
+  {
+    owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    name: String,
+    avatar: String,
+    content: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const projectSchema = new Schema(
+  {
+    projectTitle: {
+      type: String,
+      required: true,
+    },
+    projectDescription: {
+      type: String,
+      required: true,
+    },
+    projectCategory: {
+      category: {
+        type: String,
+        enum: [
+          '3DPrinting',
+          'Art',
+          'Automotive',
+          'Beekeeping',
+          'CandleMaking',
+          'Coding',
+          'Cosplay',
+          'Crafts',
+          'Crochet',
+          'DIYBeautyAndSkincare',
+          'DIYEducationalToys',
+          'DIYElectronicsKits',
+          'DIYFurniture',
+          'DIYGiftsAndHandmadeCards',
+          'DIYHomeDecor',
+          'Electronics',
+          'Fashion',
+          'Food',
+          'GameDevelopment',
+          'Gardening',
+          'GraphicDesign',
+          'HomeImprovement',
+          'Jewelry',
+          'Knitting',
+          'Leatherworking',
+          'Metalworking',
+          'ModelBuilding',
+          'Music',
+          'Other',
+          'OutdoorAndSurvivalSkills',
+          'Papercrafts',
+          'Photography',
+          'Pottery',
+          'Printmaking',
+          'Quilting',
+          'Recycling',
+          'Repurposing',
+          'Robotics',
+          'Sculpture',
+          'Sewing',
+          'Upcycling',
+          'VirtualRealityCreations',
+          'Weaving',
+          'Woodcarving',
+          'Woodturning',
+          'Woodworking',
+        ],
+      },
+    },
+    otherCategory: [String],
+    buildPictures: [String],
+    materialsNeeded: [String],
+    externalLinks: [String],
+    buildInstructions: {
+      type: [String],
+      required: true,
+    },
+    buildTime: Number,
+    difficulty: Number,
+    comments: [projectComment],
+    owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
+    ownerName: String,
+    ownerAvatar: String,
+    dateBuilt: Date,
+    liked: [{ type: Schema.Types.ObjectId, ref: 'Profile' }],
+    rating: [Number],
+    visible: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Recipe = mongoose.model('Project', projectSchema);
+export { Project };
