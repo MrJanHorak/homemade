d92f0aebca84d320919e0c190763124423e657ef
MrJanHorak <janny_b_goode@hotmail.com>
2023-11-20 18:18:54 -0500

    stub up routes and middleware

---
+++ routes/projects.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as projectCtrl from "../controllers/projects.js";
+import { isLoggedIn } from "../middleware/middleware.js";
+
+const router = Router();
+
+// localhost:3000/recipes/
+router.get("/", projectCtrl.index);
+router.get("/new", isLoggedIn, projectCtrl.new);
+router.get("/:id", projectCtrl.show);
+router.get("/:id/edit", isLoggedIn, projectCtrl.edit);
+router.post("/", isLoggedIn, projectCtrl.create);
+router.post("/:id/comments", isLoggedIn, projectCtrl.addComment);
+router.put("/:id", isLoggedIn, projectCtrl.update);
+router.delete("/:id", isLoggedIn, projectCtrl.delete);
+router.delete("/:id/comments/:commentId", isLoggedIn, projectCtrl.deleteComment);
+
+export { router };
