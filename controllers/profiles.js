3d5e972a1ecd329619a7b3b798dd7b8e02877cd6
MrJanHorak <janny_b_goode@hotmail.com>
2023-11-24 21:32:08 -0500

    add edit and update profile controller functions

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -44,4 +44,57 @@ const show = (req, res) => {
     });
 };
 
-export { index, show };
+const edit = (req, res) => {
+  Profile.findById(req.params.id)
+    .then((profile) => {
+      Profile.findById(req.user.profile._id).then((self) => {
+        const role = self.role;
+        const isSelf = self._id.equals(profile._id);
+        res.render('profiles/edit', {
+          self,
+          isSelf,
+          role,
+          title: 'Edit Profile',
+          profile,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/profiles');
+    });
+}
+
+const update = (req, res) => {
+  Profile.findById(req.params.id)
+    .then((profile) => {
+      profile.name = req.body.name;
+      profile.avatar = req.body.avatar;
+      profile.description = req.body.description;
+      profile.location = req.body.location;
+      profile.website = req.body.website;
+      profile.social.facebook = req.body.facebook;
+      profile.social.twitter = req.body.twitter;
+      profile.social.linkedin = req.body.linkedin;
+      profile.social.instagram = req.body.instagram;
+      profile.social.youtube = req.body.youtube;
+      profile.social.snapchat = req.body.snapchat;
+      profile.social.pinterest = req.body.pinterest;
+      profile.social.reddit = req.body.reddit;
+      profile.social.tiktok = req.body.tiktok;
+      profile.social.twitch = req.body.twitch;
+      profile.social.discord = req.body.discord;
+      profile.social.github = req.body.github;
+      profile.social.other = req.body.other;
+      profile.skills = req.body.skills;
+      profile.save();
+      res.redirect(`/profiles/${profile._id}`);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/profiles');
+    });
+}
+
+
+export { index, show, edit, update };
--- routes/profiles.js
+++ routes/profiles.js
@@ -6,5 +6,7 @@ const router = Router();
 
 router.get('/', profilesCtrl.index);
 router.get('/:id', profilesCtrl.show);
+router.get('/:id/edit', isLoggedIn, profilesCtrl.edit);
+router.put('/:id', isLoggedIn, profilesCtrl.update);
 
 export { router };
